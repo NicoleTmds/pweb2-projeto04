d35017cf52ae7d2993bff52c48bc190105637030
NicoleTmds <nicole.tamarindo21@gmail.com>
2024-12-15 22:46:43 -0300

    Swagger/Cache/Pilha

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const helmet = require('helmet')
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 // Routes import
 const routes = require('./routes')
@@ -34,10 +36,34 @@ app.use(logger('dev'))
 app.use(helmet())
 app.use(cookieParser())
 
+// Swagger options
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'API documentation with Swagger',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`, // Atualize conforme o servidor em produção
+        description: 'Local server',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Caminho para os arquivos de rotas onde estão as anotações do Swagger
+}
+
+// Swagger setup
+const swaggerSpec = swaggerJSDoc(swaggerOptions)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 // Routes middleware
 app.use('/api', routes)
 
 // Port listener
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
+  console.log(`Swagger documentation available at http://localhost:${port}/api-docs`)
 })
